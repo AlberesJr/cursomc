959c44eb8a807b8ae68e62cdf482d1afeb796fde
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-12 12:09:44 -0300

    Testando REST

--- src/main/java/com/alberes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alberes/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/Categorias")
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
-
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public String listar() {
-		return "REST está funcionando";
+		return "REST está funcionando!";
 	}
+
 }
