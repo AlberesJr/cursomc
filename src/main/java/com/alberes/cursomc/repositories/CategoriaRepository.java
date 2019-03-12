2ce492ce69f68524ac36ad97f4b48d2d728b3300
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-12 13:35:19 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/alberes/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.alberes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alberes.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+}
