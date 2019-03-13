874be36272d351459ae4c069456d089427399bba
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-13 14:48:22 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/alberes/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.alberes.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.alberes.cursomc.services.exceptions.ObjectNotFoudException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoudException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoudException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
--- src/main/java/com/alberes/cursomc/services/CategoriaService.java
+++ src/main/java/com/alberes/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.alberes.cursomc.domain.Categoria;
 import com.alberes.cursomc.repositories.CategoriaRepository;
+import com.alberes.cursomc.services.exceptions.ObjectNotFoudException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoudException("Objeto não encontrado! Id: "
+					+ id + ", Tipo: " +Categoria.class.getName() );
+		}
 		return obj;
 	}
 	
