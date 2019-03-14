e108876ea3dd47c215dbce3fec2af9e980354d06
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-14 12:48:58 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/alberes/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.alberes.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alberes.cursomc.domain.Pedido;
+import com.alberes.cursomc.repositories.PedidoRepository;
+import com.alberes.cursomc.services.exceptions.ObjectNotFoudException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoudException("Objeto não encontrado! Id: "
+					+ id + ", Tipo: " +Pedido.class.getName() );
+		}
+		return obj;
+	}
+	
+}
