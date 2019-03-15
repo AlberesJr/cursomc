01b3e5781b7df6db922bbf49969c4db17ae31fb6
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-15 13:19:27 -0300

    PUT, DELETE e GET em Cliente

---
+++ src/main/java/com/alberes/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,60 @@
+package com.alberes.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.alberes.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Prenchimento obrigatório")
+	@Length(min=5, max=120, message="Não pode ser menor que 5 e maior que 120")
+	private String nome;
+	
+	@NotEmpty
+	@Email
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+public ClienteDTO(Cliente cli) {
+		id = cli.getId();
+		nome = cli.getNome();
+		email =  cli.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/alberes/cursomc/services/ClienteService.java
+++ src/main/java/com/alberes/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.alberes.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alberes.cursomc.domain.Cliente;
+import com.alberes.cursomc.dto.ClienteDTO;
 import com.alberes.cursomc.repositories.ClienteRepository;
+import com.alberes.cursomc.services.exceptions.DataIntegrityException;
 import com.alberes.cursomc.services.exceptions.ObjectNotFoudException;
 
 @Service
@@ -14,12 +22,46 @@ public class ClienteService {
 	private ClienteRepository repo;
 	
 	public Cliente find(Integer id) {
-		Cliente obj = repo.findOne(id);
-		if (obj == null) {
+		Cliente cli = repo.findOne(id);
+		if (cli == null) {
 			throw new ObjectNotFoudException("Objeto não encontrado! Id: "
 					+ id + ", Tipo: " + Cliente.class.getName() );
 		}
-		return obj;
+		return cli;
+	}
+	
+	public Cliente update(Cliente cli) {
+		Cliente newCli = find(cli.getId());
+		updateData(newCli, cli);
+		return repo.save(newCli);
+	}
+
+	public void delete(Integer id) {
+		try {
+			repo.delete(find(id));
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que contém produtos.");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO cliDto) {
+		//return new Cliente(catDto.getId(), catDto.getNome());
+		return new Cliente(cliDto.getId(), cliDto.getNome(), cliDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newCli, Cliente cli) {
+		newCli.setNome(cli.getNome());
+		newCli.setEmail(cli.getEmail());
+		
 	}
 	
 }
