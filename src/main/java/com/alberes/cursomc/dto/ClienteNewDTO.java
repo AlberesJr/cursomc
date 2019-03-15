2d44a32bd5f10b600fe533fd9b130560274f2f1c
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-15 16:14:23 -0300

    validacao customizada de cpf ou cnpj no insert de cliente

--- src/main/java/com/alberes/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/alberes/cursomc/dto/ClienteNewDTO.java
@@ -2,24 +2,45 @@ package com.alberes.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.alberes.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@NotEmpty(message = "Preenchimeto obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
+
+	@NotEmpty(message = "Preenchimento automático")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String email;
+
+	@NotEmpty(message = "Preenchimento automático")
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+
+	@NotEmpty(message = "Preenchimento automático")
 	private String logradouro;
+
+	@NotEmpty(message = "Preenchimento automático")
 	private String numero;
+
 	private String complemento;
 	private String bairro;
+
+	@NotEmpty(message = "Preenchimento automático")
 	private String cep;
-	
+
+	@NotEmpty(message = "Preenchimento automático")
 	private String telefone1;
+
 	private String telefone2;
 	private String telefone3;
-	
+
 	private Integer cidadeId;
 
 	public String getNome() {
@@ -125,7 +146,5 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-	
-	
 
 }
