7f1419d97f4fdd4ffdde888c07e3936bb380e324
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-20 13:50:25 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/alberes/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.alberes.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.alberes.cursomc.domain.Cliente;
+import com.alberes.cursomc.repositories.ClienteRepository;
+import com.alberes.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfil());
+	}
+
+}
