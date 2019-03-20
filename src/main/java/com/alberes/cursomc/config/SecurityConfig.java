2f23d6ebbabd81978961eb8ac72aef0baf0c8ae6
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-20 14:46:28 -0300

    Implementando autorizacao

--- src/main/java/com/alberes/cursomc/config/SecurityConfig.java
+++ src/main/java/com/alberes/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alberes.cursomc.security.JWTAuthenticationFilter;
+import com.alberes.cursomc.security.JWTAuthorizationFilter;
 import com.alberes.cursomc.security.JWTUtil;
 
 @Configuration
@@ -54,6 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
