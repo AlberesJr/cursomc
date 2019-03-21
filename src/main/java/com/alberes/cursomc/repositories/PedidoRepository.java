bee603e2f63bc824fd5514564ff9545bfb7b6746
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-21 11:26:26 -0300

    Restricao de pedidos, cliente so recupera seus pedidos

--- src/main/java/com/alberes/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/alberes/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.alberes.cursomc.repositories;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.alberes.cursomc.domain.Cliente;
 import com.alberes.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+	@Transactional
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/alberes/cursomc/services/PedidoService.java
+++ src/main/java/com/alberes/cursomc/services/PedidoService.java
@@ -3,9 +3,13 @@ package com.alberes.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.alberes.cursomc.domain.Cliente;
 import com.alberes.cursomc.domain.ItemPedido;
 import com.alberes.cursomc.domain.PagamentoComBoleto;
 import com.alberes.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.alberes.cursomc.domain.enums.EstadoPagamento;
 import com.alberes.cursomc.repositories.ItemPedidoRepository;
 import com.alberes.cursomc.repositories.PagamentoRepository;
 import com.alberes.cursomc.repositories.PedidoRepository;
+import com.alberes.cursomc.security.UserSS;
+import com.alberes.cursomc.services.exceptions.AuthorizationException;
 import com.alberes.cursomc.services.exceptions.ObjectNotFoudException;
 
 @Service
@@ -31,8 +37,7 @@ public class PedidoService {
 	@Autowired
 	private ClienteService clienteService;
 	@Autowired
-	private EmailService emailService;
-	
+	private EmailService emailService;	
 
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
@@ -66,4 +71,15 @@ public class PedidoService {
 		return ped;
 	}
 
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cli = clienteService.find(user.getId());
+		return repo.findByCliente(cli, pageRequest);
+	}
+	
 }
