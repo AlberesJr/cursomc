5e9ce4e4ad0b95fd23d8ce65c634c6ff36baa505
alberesjr22@gmail.com <alberes.nascimento@grupoavantia.com.br>
2019-03-21 15:55:03 -0300

    Salvando URL da imagem no cliente

--- src/main/java/com/alberes/cursomc/services/ClienteService.java
+++ src/main/java/com/alberes/cursomc/services/ClienteService.java
@@ -113,7 +113,16 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		URI uri = s3Service.uploadFile(multipartFile);
+		Cliente cliente = repo.findOne(user.getId());
+		cliente.setImageUrl(uri.toString());
+		repo.save(cliente);
+		return uri;
 	}
 	
 }
